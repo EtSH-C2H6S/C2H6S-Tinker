62d58c6604cc8ea7539402a6fad51a06a2330487
EtSH-C2H6S <2130679254@qq.com>
2024-09-21 16:07:13 +0800

    fix

--- src/main/java/com/c2h6s/etshtinker/Modifiers/ReactiveResonateArmor.java
+++ src/main/java/com/c2h6s/etshtinker/Modifiers/ReactiveResonateArmor.java
@@ -4,14 +4,15 @@ import com.c2h6s.etshtinker.Entities.CustomSonicBoomEntity;
 import com.c2h6s.etshtinker.Modifiers.modifiers.etshmodifieriii;
 import com.c2h6s.etshtinker.init.etshtinkerEntity;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.phys.Vec3;
-import slimeknights.tconstruct.library.modifiers.ModifierEntry;
-import slimeknights.tconstruct.library.tools.context.EquipmentContext;
-import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import slimeknights.tconstruct.TConstruct;
+import slimeknights.tconstruct.library.modifiers.modules.technical.ArmorLevelModule;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
+import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
 
 import static com.c2h6s.etshtinker.util.vecCalc.getScatteredVec3;
 
@@ -20,27 +21,41 @@ public class ReactiveResonateArmor extends etshmodifieriii {
     public boolean isNoLevels() {
         return true;
     }
-
-    @Override
-    public float modifierDamageTaken(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount, boolean isDirectDamage) {
-        Entity entity =context.getEntity();
-        if (entity instanceof LivingEntity) {
-            CustomSonicBoomEntity entity2 = new CustomSonicBoomEntity(etshtinkerEntity.sonic_boom.get(), entity.level);
-            entity2.setOwner(entity);
-            entity2.direction = getScatteredVec3(new Vec3(0, 1, 0), 87);
-            entity2.damage = amount;
-            entity2.setPos(entity.getX(), entity.getY() +0.5*entity.getBbHeight(), entity.getZ());
-            entity2.range = 8;
-            entity2.level.addFreshEntity(entity2);
-            CustomSonicBoomEntity entity1 = new CustomSonicBoomEntity(etshtinkerEntity.sonic_boom.get(), entity.level);
-            entity1.setOwner(entity);
-            entity1.direction = getScatteredVec3(new Vec3(0, -1, 0), 87);
-            entity1.damage = amount;
-            entity1.setPos(entity.getX(), +0.5*entity.getBbHeight(), entity.getZ());
-            entity1.level.addFreshEntity(entity1);
-            entity1.playSound(SoundEvents.WARDEN_SONIC_BOOM, 1, 1);
-            entity1.range = 8;
-        }
-        return amount*0.75f;
+    private static final TinkerDataCapability.TinkerDataKey<Integer> key = TConstruct.createKey("reactive_resonate_armor");
+    public ReactiveResonateArmor(){
+        MinecraftForge.EVENT_BUS.addListener(this::livinghurtevent);
     }
+    @Override
+    protected void registerHooks(ModuleHookMap.Builder builder) {
+        super.registerHooks(builder);
+        builder.addModule(new ArmorLevelModule(key, false, (TagKey)null));
+    }
+    private void livinghurtevent(LivingHurtEvent event) {
+        LivingEntity living = event.getEntity();
+        float amount = event.getAmount();
+        living.getCapability(TinkerDataCapability.CAPABILITY).ifPresent((holder) -> {
+            int level = holder.get(key, 0);
+            if (level > 0) {
+                if (living instanceof LivingEntity) {
+                    CustomSonicBoomEntity entity2 = new CustomSonicBoomEntity(etshtinkerEntity.sonic_boom.get(), living.level);
+                    entity2.setOwner(living);
+                    entity2.direction = getScatteredVec3(new Vec3(0, 1, 0), 87);
+                    entity2.damage = amount;
+                    entity2.setPos(living.getX(), living.getY() +0.5* living.getBbHeight(), living.getZ());
+                    entity2.range = 8;
+                    entity2.level.addFreshEntity(entity2);
+                    CustomSonicBoomEntity entity1 = new CustomSonicBoomEntity(etshtinkerEntity.sonic_boom.get(), living.level);
+                    entity1.setOwner(living);
+                    entity1.direction = getScatteredVec3(new Vec3(0, -1, 0), 87);
+                    entity1.damage = amount;
+                    entity1.setPos(living.getX(), +0.5* living.getBbHeight(), living.getZ());
+                    entity1.level.addFreshEntity(entity1);
+                    entity1.playSound(SoundEvents.WARDEN_SONIC_BOOM, 1, 1);
+                    entity1.range = 8;
+                }
+                event.setAmount(event.getAmount()*0.75f);
+            }
+        });
+    }
+
 }
--- src/main/java/com/c2h6s/etshtinker/Modifiers/perfectArmor.java
+++ src/main/java/com/c2h6s/etshtinker/Modifiers/perfectArmor.java
@@ -1,17 +1,43 @@
 package com.c2h6s.etshtinker.Modifiers;
 
 import com.c2h6s.etshtinker.Modifiers.modifiers.etshmodifieriii;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.EquipmentSlot;
-import slimeknights.tconstruct.library.modifiers.ModifierEntry;
-import slimeknights.tconstruct.library.tools.context.EquipmentContext;
-import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import slimeknights.tconstruct.TConstruct;
+import slimeknights.tconstruct.library.modifiers.modules.technical.ArmorLevelModule;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
+import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
 
-import static com.c2h6s.etshtinker.etshtinker.EtSHrnd;
 
 public class perfectArmor extends etshmodifieriii {
+    private static final TinkerDataCapability.TinkerDataKey<Integer> key = TConstruct.createKey("perfect_armor");
+    public perfectArmor(){
+        MinecraftForge.EVENT_BUS.addListener(this::livingHurtevent);
+    }
+
     @Override
-    public float modifierDamageTaken(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount, boolean isDirectDamage) {
-        return EtSHrnd().nextInt(100)==0?100:0;
+    protected void registerHooks(ModuleHookMap.Builder builder) {
+        super.registerHooks(builder);
+        builder.addModule(new ArmorLevelModule(key, false, (TagKey)null));
+    }
+
+    private void livingHurtevent(LivingHurtEvent event) {
+        float amount = event.getAmount();
+        Entity entity = event.getEntity();
+        if (entity instanceof LivingEntity living){
+            living.getCapability(TinkerDataCapability.CAPABILITY).ifPresent((holder) -> {
+                int level = holder.get(key, 0);
+                if (level > 0) {
+                    float b =amount/level;
+                    if (b<=4){
+                        event.setAmount(0);
+                    }
+                    else event.setAmount(10);
+                }
+            });
+        }
     }
 }
